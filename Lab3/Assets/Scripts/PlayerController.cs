484fad10f7d659ef6df28a3d49fde888159adad6
Etzernal <chan.jielin@dhs.sg>
2021-07-08 07:55:04 +0800

    Adds Lab 3

---
+++ Lab3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator marioAnimator;
+    private AudioSource marioAudioSource;
+
+
+    public float speed;
+    private Rigidbody2D marioBody;
+    public float maxSpeed = 10;
+    private bool onGroundState = true;
+    public float upSpeed = 15;
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+
+    public Transform enemyLocation;
+    public Text scoreText;
+    public int score = 0;
+    private bool countScoreState = false;
+    public MenuController menu;
+
+
+    void Start()
+    {
+        // Set to be 30 FPS
+        Application.targetFrameRate = 30;
+        marioBody = GetComponent<Rigidbody2D>();
+        marioSprite = GetComponent<SpriteRenderer>();
+        marioAnimator = GetComponent<Animator>();
+        marioAudioSource = GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+        marioAnimator.SetFloat("xSpeed", Mathf.Abs(marioBody.velocity.x));
+
+        // toggle state
+        if (Input.GetKeyDown("a") && faceRightState)
+        {
+            faceRightState = false;
+            marioSprite.flipX = true;
+            if (Mathf.Abs(marioBody.velocity.x) > 1.0)
+                marioAnimator.SetTrigger("onSkid");
+
+        }
+
+        if (Input.GetKeyDown("d") && !faceRightState)
+        {
+            faceRightState = true;
+            marioSprite.flipX = false;
+            if (Mathf.Abs(marioBody.velocity.x) > 1.0)
+                marioAnimator.SetTrigger("onSkid");
+        }
+
+        
+        // when jumping, and Gomba is near Mario and we haven't registered our score
+        if (!onGroundState && countScoreState)
+        {
+            if (Mathf.Abs(transform.position.x - enemyLocation.position.x) < 0.5f)
+            {
+                countScoreState = false;
+                score++;
+                Debug.Log(score);
+            }
+        }
+
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+            marioAnimator.SetBool("onGround", onGroundState);
+            countScoreState = true; //check if Gomba is underneath
+        }
+
+    }
+
+    // FixedUpdate may be called once per frame. See documentation for details.
+    void FixedUpdate() {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            // stop
+            marioBody.velocity = Vector2.zero;
+        }
+
+        // dynamic rigidbody
+        moveHorizontal = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            if (marioBody.velocity.magnitude < maxSpeed)
+                marioBody.AddForce(movement * speed);
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            onGroundState = true; // back on ground
+            marioAnimator.SetBool("onGround", onGroundState );
+            countScoreState = false; // reset score state
+            scoreText.text = "Score: " + score.ToString();
+        };
+
+        if (col.gameObject.CompareTag("Obstacles") && Mathf.Abs(marioBody.velocity.y) < 0.01f)
+        {
+            onGroundState = true; // back on ground
+            marioAnimator.SetBool("onGround", onGroundState);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with Gomba!");
+            score = 0;
+            scoreText.text = "Score: " + score.ToString();
+            transform.position = new Vector3(0,0,0);
+            marioBody.velocity = new Vector3(0, 0, 0);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            //menu.Restart();
+        }
+    }
+
+    void PlayJumpSound()
+    {
+        marioAudioSource.PlayOneShot(marioAudioSource.clip);
+    }
+
+}
